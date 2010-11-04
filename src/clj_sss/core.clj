92d6cef8519aa19866ee46d62dc9297d79a7e0b7
mortuosplango <holger@music-nerds.net>
2010-11-04 15:26:28 +0100

    init

---
+++ src/clj_sss/core.clj
@@ -0,0 +1,1 @@
+(ns clj-sss.core)
---
+++ test/clj_sss/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-sss.test.core
+  (:use [clj-sss.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
